0148aa34de32a78b34967c432efbfa24b71de190
Victor <mr>
2025-04-11 17:41:59 +0300

    Hotfix Init

--- Converter/Controllers /TabbarController.swift
+++ Converter/Controllers /TabbarController.swift
@@ -16,10 +16,10 @@ final class TabBarController: UITabBarController {
         case settings
     }
     
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-        configure()
-    }
+    init() {
+            super.init(nibName: nil, bundle: nil)
+            configure()
+        }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
